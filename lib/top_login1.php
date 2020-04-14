f139e4a722e9d86d3ae6a1063d133dfc94bd0375
mia2628 <50148752+mia2628@users.noreply.github.com>
2020-04-14 23:37:26 +0900

    Add files via upload

---
+++ lib/top_login1.php
@@ -0,0 +1,19 @@
+    <div id="logo"><a href="C:\APM_Setup\htdocs\index.php"><img src="C:\APM_Setup\htdocs\lib\study1.png" border="0"></a></div>
+	<div id="moto"><img src=""></div>
+	<div id="top_login">
+<?
+    if(!$userid)
+	{
+?>
+          <a href="./login/login_form.php">로그인</a> | <a href="./member/member_form.php">회원가입</a>
+<?
+	}
+	else
+	{
+?>
+		<?=$usernick?> (level:<?=$userlevel?>) | 
+		<a href="./login/logout.php">로그아웃</a> | <a href="./login/member_form_modify.php">정보수정</a>
+<?
+	}
+?>
+	 </div>
---
+++ lib/top_menu1.php
@@ -0,0 +1,2 @@
+<div class="menus"><a href="./memo/memo.php"><img src="./img/hello1.jpg" border="0"></a></div>
+<div class="menus"><a href="./greet/list.php"><img src="./img/hello.jpg" border="0"></a></div>
